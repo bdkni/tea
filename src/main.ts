2cdbb137e8c496b6256a1410ac69cdb5a70af1be
Irina Budiakina <ibudiakina@gmail.com>
2023-03-14 20:37:43 +0300

    changes for prod

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,8 @@
-/// <reference types="@angular/localize" />
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import {environment} from "./environments/environment";
 
-if(environment.production)
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
 
